313fe2ecc4bff9870fd28d20644f49377cc0c17c
ksrinivasareddy <112409060+ksrinivasareddy@users.noreply.github.com>
2024-07-17 11:25:37 +0530

    Add files via upload

--- operators_in_mysql.sql
+++ operators_in_mysql.sql
@@ -12,4 +12,48 @@ SELECT NAME,AGE,(SALARY/2) AS NEWDATA FROM EMPLOYEE
 SELECT NAME,AGE,(AGE%3) AS NEWDATA FROM EMPLOYEE
 
 
--- 
+-- comparision operatora
+select  * from employee where salary > 50000
+select  * from employee where salary < 50000
+select  * from employee where salary >= 45000
+select  * from employee where salary <= 45000
+select  * from employee where salary != 50000
+
+-- logical operators
+select * from employee where age = 24 and salary > 45000
+select * from employee where age = 24 or salary > 45000
+
+
+-- null operators
+
+set sql_safe_updates = 0
+
+alter table employee add column mobileno bigint
+update employee set mobileno = 656565656 where salary > 50000
+select * from employee where mobileno is null
+select * from employee where mobileno is not null
+
+
+-- between operator
+select * from employee where age between 22 and 25 
+
+
+-- in operator
+select * from employee where salary in(30000,50000)
+
+
+-- case operator
+
+select *,
+case
+	when salary <= 10000 then 0.01
+    when salary <= 20000 then 0.02
+    when salary >= 30000 then 0.04
+    else 0
+end as bonus
+from employee;
+    
+-- string operators
+
+select name,age, concat(name," (",age," in age )") as data from employee
+
